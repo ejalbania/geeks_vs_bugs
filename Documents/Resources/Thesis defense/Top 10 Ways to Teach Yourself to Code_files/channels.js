74d9cae862aa7466c41e540e2c4853c19cc4c795
Emmanuel <ej.albania@gmail.com>
2016-10-13 23:37:53 +0800

    References

---
+++ Documents/Resources/Thesis defense/Top 10 Ways to Teach Yourself to Code_files/channels.js
@@ -0,0 +1,21 @@
+
+
+
+var oo_profile={
+ tokenType : "1",
+ tracking : "318332,318430,319512",
+ tags : "25320,30952,437522",
+ tagcloud : [
+  { tag: "25320", weight: 100, token: "318332"}, 
+  { tag: "30952", weight: 100, token: "318430"}, 
+  { tag: "437522", weight: 100, token: "319512"}
+  ],
+ pixels : [
+  { url: "https://pixel.rubiconproject.com/tap.php?s=oz&v=318332||318430||319512||"}
+  ]
+};
+
+
+try {
+oz_onPixelsLoaded(oo_profile);
+} catch(ignore) {}
